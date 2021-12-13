b301251c0a75d08220da8c9b50d698b24f6714c5
Hannah McCullough <hmac091@gmail.com>
2021-12-13 15:30:24 -0700

    completed assignment

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Links(props) {
+    return (
+        <div>
+            <h3>Links</h3>
+            <a href={props.github}>GitHub</a>
+            <a href={props.linkedin}>LinkedIn</a>
+        </div>
+    );
+}
+
+export default Links;
